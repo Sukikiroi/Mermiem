a2c08e34bb37b3ef76fbb90328da040545a3af7a
kaddour abdelaziz <47844266+Sukikiroi@users.noreply.github.com>
2020-08-04 19:15:29 +0000

    Update Prediction.js

--- src/Prediction.js
+++ src/Prediction.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import TextField from '@material-ui/core/TextField';
-import Menu2 from './Menu2';
+import Menu from './Menu';
 import Button from '@material-ui/core/Button';
 import Card from '@material-ui/core/Card';
 import Icon from '@material-ui/core/Icon';
